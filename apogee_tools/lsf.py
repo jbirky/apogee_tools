16531aaed99df63cc8a732119c826e7c97211db0
jbirky <jbirky@ucsd.edu>
2017-07-11 19:06:18 +0200

    remove bovy dependency

--- apogee_tools/lsf.py
+++ apogee_tools/lsf.py
@@ -1,4 +1,6 @@
-import apogee.spec.lsf as lsf
+# import apogee.spec.lsf as lsf
+
+# do we wan to copy & paste bovy code, or make it a dependency?
 
 
 #Instrument lsf - Jo Bovy code: https://github.com/jobovy/apogee
