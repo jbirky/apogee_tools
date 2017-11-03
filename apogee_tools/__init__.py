9b1662b8e7ab16dcf57de93d6aef960e3352d222
jbirky <jbirky@ucsd.edu>
2017-11-03 09:56:42 -0700

    fix info.features import

--- apogee_tools/__init__.py
+++ apogee_tools/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from .core import Spectrum
 from .spec_tools import loadGrid, _rvShift, calcScale, compareSpectra, subtractContinuum, readModels, getModel, plotModel
 from .search import searchStars, download, multiParamSearch, returnSimbadParams
-from .info import lines
+from .info.features import lines
 from .model import makeModel, returnModelFit
 from .telluric import applyTelluric
 from .synthesize_grid import interpolateGrid
