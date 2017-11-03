a7d401acd897ad8546834e9ff96ede91b7772962
jbirky <jbirky@ucsd.edu>
2017-11-03 11:38:26 -0700

    test makeModel function

--- test_tools.py
+++ test_tools.py
@@ -26,22 +26,33 @@ if __name__ == '__main__':
 # Test Model functions
 
 	params = {'teff': 3051, 'logg': 5.2, 'z': -0.25, 'vsini': 10., 'rv': -12, 'alpha': 0.8}
-	labels = [params['teff'], params['logg'], params['z']]
 
-	interp_sp = ap.interpolateGrid(labels=labels)
+# Replicate Chris T's test script
 
-	rv_sp   = ap.spec_tools.rvShiftSpec(interp_sp, rv=params['rv'])
+	# labels = [params['teff'], params['logg'], params['z']]
 
-	rot_sp  = ap.applyVsini(rv_sp, vsini=params['vsini'])
+	# interp_sp = ap.interpolateGrid(labels=labels)
 
-	tell_sp = ap.applyTelluric(rot_sp)
+	# rv_sp   = ap.spec_tools.rvShiftSpec(interp_sp, rv=params['rv'])
 
-	plt.figure(1, figsize=(10,6))  
-	plt.plot(interp_sp.wave, interp_sp.flux, label=r'Teff = %s, logg = %s, Fe/H = %s'%(params['teff'], params['logg'], params['z']))
-	plt.plot(rv_sp.wave, rv_sp.flux, label=r'RV (%s km/s)'%(params['rv']))
-	plt.plot(rot_sp.wave, rot_sp.flux, label=r'RV + rot (%s km/s)'%(params['vsini']))
-	plt.plot(tell_sp.wave, tell_sp.flux, label=r'RV + rot + telluric')
-	plt.xlim(15678, 15694)
-	plt.ylim(0.7, 1.1)
-	plt.legend(frameon=False)
-	plt.show()
+	# rot_sp  = ap.applyVsini(rv_sp, vsini=params['vsini'])
+
+	# lsf_sp  = ap.convolveLsf(rot_sp, fiber=40)
+
+	# tell_sp = ap.applyTelluric(lsf_sp)
+
+	# plt.figure(1, figsize=(10,6))  
+	# plt.plot(interp_sp.wave, interp_sp.flux, label=r'Teff = %s, logg = %s, Fe/H = %s'%(params['teff'], params['logg'], params['z']))
+	# plt.plot(rv_sp.wave, rv_sp.flux, label=r'RV (%s km/s)'%(params['rv']))
+	# plt.plot(rot_sp.wave, rot_sp.flux, label=r'RV + rot (%s km/s)'%(params['vsini']))
+	# plt.plot(lsf_sp.wave, lsf_sp.flux, label=r'RV + rot + lsf')
+	# plt.plot(tell_sp.wave, tell_sp.flux, label=r'RV + rot + lsf + telluric')
+	# plt.xlim(15678, 15694)
+	# plt.ylim(0.7, 1.1)
+	# plt.legend(frameon=False)
+	# plt.show()
+
+# Test makeModel function (found in model.py), which is the module containing the test code above
+
+	ap.makeModel(params=params, fiber=40, plot=True, xrange=[15678,15694])
+
