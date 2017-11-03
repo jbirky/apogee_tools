5dbe5dc94ee2ec4c540ac1ff97cd4c5b7c066e81
jbirky <jbirky@ucsd.edu>
2017-11-03 03:15:38 -0700

    update imports with new modules

--- apogee_tools/__init__.py
+++ apogee_tools/__init__.py
@@ -1,7 +1,13 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from .core import *
-from .spec_tools import *
-#from .lsf import *
-from .search import *
+from .core import Spectrum
+from .spec_tools import loadGrid, _rvShift, calcScale, compareSpectra, subtractContinuum, readModels, getModel, plotModel
+from .search import searchStars, download, multiParamSearch, returnSimbadParams
 from .info import *
+from .model import Model
+from .telluric import applyTelluric
+from .synthesize_grid import interpolateGrid
+from .lsf_function import convolveLsf
+
+# import .apogee_hack.spec.lsf as lsf
+# from .apogee_hack.spec.plot import apStarWavegrid
