eb146f83a6dfb5768d64612c05900f9833104873
jbirky <jbirky@ucsd.edu>
2017-11-14 12:55:45 -0800

    replace NaNs in flux array

--- apogee_tools/continuum.py
+++ apogee_tools/continuum.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import copy
 
+
 def continuum(data, mdl, **kwargs):
 
 	"""
@@ -12,15 +13,26 @@ def continuum(data, mdl, **kwargs):
 	mdl: the model being corrected. 
 	contfitdeg: the degree of the fitting polynomial. The default vaule is 5.
 	"""
-	
-	deg = kwargs.get('deg', 5)
 
-	# interpolate the shape of the data to be the same as that of the model
-	data_int = np.interp(mdl.wave, data.wave, data.flux)
+	deg = kwargs.get('deg', 1) 
 
-	mdlcont = copy.deepcopy(mdl)
-	mdldiv  = data_int/mdl.flux
-	pcont   = np.polyfit(mdl.wave, mdldiv, deg)
+	flux_in_rng = np.where((mdl.wave > data.wave[0]) & (mdl.wave < data.wave[-1]))[0]
+	mdl_wave = mdl.wave[flux_in_rng]
+	mdl_flux = mdl.flux[flux_in_rng]
+	   
+	mdl_flux_vals = [x for x in mdl_flux if str(x) != 'nan']
+	mdl_flux = mdl_flux/max(mdl_flux_vals)
+	mdl_flux = [1 if str(x) == 'nan' else x for x in mdl_flux]
+
+	mdl_int = np.interp(data.wave, mdl_wave, mdl_flux)
+	mdlcont = ap.Spectrum(wave=mdl_wave, flux=mdl_flux)
+	mdldiv  = data.flux/mdl_int
+
+	mdldiv_vals = [x for x in mdldiv if str(x) != 'nan']
+	mdldiv = [np.mean(mdldiv_vals) if str(x) == 'nan' else x for x in mdldiv]
+	pcont  = np.polyfit(data.wave, mdldiv, deg)
+	    
 	mdlcont.flux *= np.polyval(pcont, mdlcont.wave)
 
 	return mdlcont
+
