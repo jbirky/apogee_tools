c03d43e6eff046ce6674bc2a56c32e2a30d5aed2
jbirky <jbirky@ucsd.edu>
2017-11-16 12:02:22 -0800

    add imports of new files

--- apogee_tools/__init__.py
+++ apogee_tools/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from .core import Spectrum
-from .spec_tools import loadGrid, _rvShift, calcScale, compareSpectra, subtractContinuum, readModels, getModel, plotModel
+from .spec_tools import calcScale, compareSpectra, subtractContinuum, integralResample
 from .search import searchStars, searchVisits, download, multiParamSearch, returnSimbadParams, returnAspcapTable
 from .ap1d import get_1dspec_urls
 from .info.features import lines
@@ -11,6 +11,8 @@ from .synthesize_grid import interpolateGrid
 from .lsf_function import convolveLsf
 from .rotation_broaden import broaden, applyVsini
 from .continuum import continuum
+from .read import HDF5Convert, HDF5Interface, loadGrid, readModels, getModel
+from .rv_function import rvShift, rvShiftSpec
 
 # import .apogee_hack.spec.lsf as lsf
 # from .apogee_hack.spec.plot import apStarWavegrid
