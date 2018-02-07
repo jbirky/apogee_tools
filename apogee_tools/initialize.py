ee942765370b8e92476597aa759382f5e3912242
jbirky <jbirky@ucsd.edu>
2018-02-07 14:10:05 -0800

    pseudo files for running fit

---
+++ apogee_tools/initialize.py
@@ -0,0 +1,4 @@
+from .instrument_tools import instrument_info as info
+
+#Read info .yaml file, and choose which forward model functions to run
+
---
+++ run.py
@@ -0,0 +1,8 @@
+import apogee_tools as ap
+import os
+
+if __name__ == "__main__":
+
+#psuedo work flow:
+	# init = ap.initialize()
+	# fit = ap.fitModel(init)
