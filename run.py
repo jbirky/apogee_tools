665b03188177fb2f3f13b0028f0e93a9e05f316a
jbirky <jbirky@ucsd.edu>
2018-04-13 15:38:21 -0700

    add model running script

--- run.py
+++ run.py
@@ -5,4 +5,8 @@ if __name__ == "__main__":
 
 #psuedo work flow:
 	# init = ap.initialize()
-	# fit = ap.fitModel(init)
+	# fit = ap.fitMCMC(init)
+
+	init_par, step_par, fiber = ap.initialize()
+
+	print(fiber)
