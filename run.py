540e865af33393e03f8bcb089a4a1fa9c5304d0c
jbirky <jbirky@ucsd.edu>
2018-04-17 15:56:10 -0700

    update

--- run.py
+++ run.py
@@ -3,10 +3,14 @@ import os
 
 if __name__ == "__main__":
 
-#psuedo work flow:
-	# init = ap.initialize()
-	# fit = ap.fitMCMC(init)
+	if 'config.yaml' not in os.listdir():
+		print('\nError: config.yaml not found in the current working directory. \
+			Using default file found inside apogee_tools.\n')
 
 	init_par, step_par, fiber = ap.initialize()
 
+	print(init_par)
+	print(step_par)
 	print(fiber)
+
+	# fit = ap.fitMCMC(init_par, step_par, fiber)
