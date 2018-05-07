741a5e21a82ba57c288dab685efaf9b1011262d7
jbirky <jbirky@ucsd.edu>
2018-05-07 14:09:48 -0700

    simplify apogee spectrum class

--- apogee_tools/core.py
+++ apogee_tools/core.py
@@ -28,16 +28,19 @@ AP_PATH = os.environ['APOGEE_DATA']
 
 class Spectrum:
 
+    """
+    Master Spectrum class.
+    """
+
     def __init__(self, **kwargs):
 
-            self.wave   = kwargs.get('wave', [])
-            self.flux   = kwargs.get('flux', [])
-            self.error  = kwargs.get('error', [0 for i in range(len(self.wave))])
-            self.ivar   = kwargs.get('ivar', [])
-            self.model  = kwargs.get('model', [])
-            self.name   = kwargs.get('name', 'Spectrum')  
-            self.params = kwargs.get('params', [])   
-            self.vsini  = kwargs.get('vsini', []) 
+            self.wave  = kwargs.get('wave', [])
+            self.flux  = kwargs.get('flux', [])
+            self.error = kwargs.get('error', [0 for i in range(len(self.wave))])
+            self.maske = kwargs.get('mask', [])
+            self.model = kwargs.get('model', [])
+            self.name  = kwargs.get('name', 'Spectrum')  
+            self.param = kwargs.get('param', [])   
 
     def mask(self, **kwargs):
 
@@ -157,189 +160,101 @@ class Spectrum:
 class APOGEE(Spectrum):
 
     """ 
-    Spectrum class for reading in apogee fits files; includes features for aspcap, apStar, and apVisit.
+    APOGEE Spectrum class for reading in apogee fits files; includes features for aspcap, apStar, and apVisit.
     Read in a file, or write in your own parameters.
     @Jessica Birky, Christian Aganze
 
     Input: 'id'   : 2MASS name. If downloaded the spectrum will be loaded from 'APOGEE_DATA' environmental variable
            'type' : aspcap, apStar, or apVisit
-    
-    Spectrum object contains parameters: 
-        wave, flux, sky, error (or error), name, continuum, params, apModel (aspcap model)
     """
 
     def __init__(self, **kwargs):  
-
-        super()__init__(self, **kwargs)
 
         self.d_type = kwargs.get('type')
 
         # Get designation of 2MASS object
         input_id  = kwargs.get('id')            
-        self.name = ap.formatDesignation(input_id)
+        name = ap.formatDesignation(input_id)
 
         if self.d_type == 'aspcap':
 
-            file_dr14 = '%s/aspcap_data/aspcapStar-r8-l31c.2-%s.fits' %(AP_PATH, spec_id)
-
-            """ ASPCAP file info:
-            HDU0: The Primary Header
-            HDU1: Spectrum array
-            HDU2: Error array
-            HDU3: Best fit spectrum
-            HDU4: ASPCAP data table
-            Information found at https://data.sdss.org/datamodel/files/APOGEE_REDUX/APRED_VERS/APSTAR_VERS/ASPCAP_VERS/RESULTS_VERS/LOCATION_ID/aspcapStar.html 
-            """
+            file_dr14 = '%s/aspcap_data/aspcapStar-r8-l31c.2-%s.fits' %(AP_PATH, name)
 
             if (os.path.exists(file_dr14) == False):
-                ap.download(self.name, type='aspcap')
+                ap.download(name, type='aspcap')
 
             openFile = fits.open(file_dr14)
 
-            self.HDU0 = openFile[0]
-            self.HDU1 = openFile[1]
-            self.HDU2 = openFile[2]
-            self.HDU3 = openFile[3]
-            self.HDU4 = openFile[4]
+            crval = openFile[1].header['CRVAL1']
+            cdelt = openFile[1].header['CDELT1']
 
-            #conversion from pixel to wavelength, info available in the hdu header
-            crval = self.HDU1.header['CRVAL1']
-            cdelt = self.HDU1.header['CDELT1']
-            wave  = np.array(pow(10, crval+cdelt*np.arange(self.HDU1.header['NAXIS1']))/10000)*u.micron #microns
+            wave  = np.array(pow(10, crval+cdelt*np.arange(openFile[1].header['NAXIS1'])))    
+            flux  = np.array(openFile[1].data)
+            error = np.array(openFile[2].data)
+            model = np.array(openFile[3].data)
+            param = openFile[4].data['PARAM'] 
 
-            # convert fluxes from  (10^-17 erg/s/cm^2/Ang) to  ( erg/s/cm^2/Mircon)
-            spectras = [1e-13*np.array(f)*u.erg/u.s/u.centimeter**2/u.micron for f in self.HDU1.data]
-                    
-            self.wave    = 10000*np.array(wave.value)
-            self.flux    = np.array(self.HDU1.data)
-            self.error   = np.array(self.HDU2.data)
-            self.apModel = np.array(self.HDU3.data)
-
-            #Obtain aspcap parameters
-            self.params = self.HDU4.data['PARAM'] 
+            super().__init__(wave=wave, flux=flux, error=error, model=model, param=param, name=name)
 
         elif self.d_type == 'apstar':
 
-            file_dr14 = '%s/apstar_data/apStar-r8-%s.fits' %(AP_PATH, spec_id)
-
-            """ APSTAR file info:
-            HDU0: master header with target information
-            HDU1: spectra: combined and individual
-            HDU2: error spectra
-            HDU3: mask spectra
-            HDU4: sky spectra
-            HDU5: sky error spectra
-            HDU6: telluric spectra
-            HDU7: telluric error spectra
-            HDU8: table with LSF coefficients
-            HDU9: table with RV/binary information
-            Information found at https://data.sdss.org/datamodel/files/APOGEE_REDUX/APRED_VERS/APSTAR_VERS/TELESCOPE/LOCATION_ID/apStar.html 
-            """
+            file_dr14 = '%s/apstar_data/apStar-r8-%s.fits' %(AP_PATH, name)
 
             if (os.path.exists(file_dr14) == False):
-                ap.download(self.name, type='apstar')
+                ap.download(name, type='apstar')
 
             openFile = fits.open(file_dr14)
-
-            self.HDU0 = openFile[0]
-            self.HDU1 = openFile[1]
-            self.HDU2 = openFile[2]
-            self.HDU3 = openFile[3]
-            self.HDU4 = openFile[4]
-            self.HDU5 = openFile[5]
-            self.HDU6 = openFile[6]
-            self.HDU7 = openFile[7]
-            self.HDU8 = openFile[8]
-            self.HDU9 = openFile[9]
-
-            master   = self.HDU0
-            spectra  = self.HDU1
-            error    = self.HDU2
-            mask     = self.HDU3
-            sky      = self.HDU4
-            sky_err  = self.HDU5
-            telluric = self.HDU6
-            tell_err = self.HDU7
-            lsf_coef = self.HDU8
-            rv_info  = self.HDU9
         
             #conversion from pixel to wavelength, info available in the hdu header
-            crval = spectra.header['CRVAL1']
-            cdelt = spectra.header['CDELT1']
-            wave  = np.array(pow(10, crval+cdelt*np.arange(spectra.header['NAXIS1']))/10000)*u.micron #microns
-            
-            # convert fluxes from  (10^-17 erg/s/cm^2/Ang) to  ( erg/s/cm^2/Mircon)
-            spectras = [1e-13*np.array(f)*u.erg/u.s/u.centimeter**2/u.micron for f in spectra.data]
-                    
-            self.wave    = 10000*np.array(wave.value)
-            self.flux    = np.array(spectra.data)
-            self.error   = np.array(error.data)
-            self.sky     = np.array(sky.data)
+            crval = openFile[1].header['CRVAL1']
+            cdelt = openFile[1].header['CDELT1']
+
+            wave  = np.array(pow(10, crval+cdelt*np.arange(openFile[1].header['NAXIS1'])))           
+            flux  = np.array(openFile[1].data)[0]
+            error = np.array(openFile[2].data)
+
+            super().__init__(wave=wave, flux=flux, error=error, name=name)
 
         elif self.d_type == 'apvisit':
 
             visit = kwargs.get('visit', 1)
-            self.file  = '%s/apvisit_data/apVisit-%s-%s.fits' %(AP_PATH, spec_id, visit)
+            file  = '%s/apvisit_data/apVisit-%s-%s.fits' %(AP_PATH, name, visit)
 
-            """ APVISIT file info:
-            HDU0: master header with target information
-            HDU1: spectra: combined and individual
-            HDU2: error
-            HDU3: mask
-            HDU4: wavelength array
-            HDU5: sky
-            HDU6: sky error
-            HDU7: telluric
-            HDU8: telluric error
-            HDU9: wavelength coeficients
-            HDU10: LSF coeficients
-            Information found at: https://data.sdss.org/datamodel/files/APOGEE_REDUX/APRED_VERS/TELESCOPE/PLATE_ID/MJD5/apVisit.html
-            """
+            if os.path.exists(file) == False:
+                ap.download(name, type='apvisit')
 
-            if os.path.exists(self.file) == False:
-                ap.download(self.name, type='apvisit')
+            openFile = fits.open(file)
 
-            hdu   = fits.open(self.file)
-            flux  = hdu[1].data
-            error = hdu[2].data
-            wave  = hdu[4].data
+            flux  = openFile[1].data
+            error = openFile[2].data
+            wave  = openFile[4].data
 
-            #Combine the data from the three chips into one list
-            self.wave = np.array(list(wave[0]) + list(wave[1]) + list(wave[2]))
-            self.flux = np.array(list(flux[0]) + list(flux[1]) + list(flux[2]))
+            # Combine the data from the three chips into one list
+            wave = np.array(list(wave[0]) + list(wave[1]) + list(wave[2]))
+            flux = np.array(list(flux[0]) + list(flux[1]) + list(flux[2]))
+            error = np.array(error.data)
 
-            self.error = np.array(error.data)
-            # self.sky    = np.array(sky.data)
+            super().__init__(wave=wave, flux=flux, error=error, name=name)
         
         elif self.d_type == 'ap1d':
-
-            """ AP1D file info:
-            HDU0: master header
-            HDU1: image (ADU) [FLOAT]
-            HDU2: error (ADU) [FLOAT]
-            HDU3: flag mask [INT*2]
-            HDU4: wavelength array
-            HDU5: wavelength coefficients array
-            Information found at: https://data.sdss.org/datamodel/files/APOGEE_REDUX/APRED_VERS/red/MJD5/ap1D.html
-            """
 
             visit = kwargs.get('visit', 1)
     
             ap1d_dir = AP_PATH + '/{}_data/' .format(self.d_type)
             
-            #look up fiber number for given 2MASS name and its visit number
-            ap_id, plates, mjds, fibers = ap.searchVisits(id_name=self.name)
+            # Look up fiber number for given 2MASS name and its visit number
+            ap_id, plates, mjds, fibers = ap.searchVisits(id_name=name)
             fiber = fibers[visit-1] #-1 for python zero indexing
             
             #get names of files for each chip
-            ChipA = ap1d_dir + 'ap1d-{}-{}-a.fits'.format(self.name, visit)
-            ChipB = ap1d_dir + 'ap1d-{}-{}-b.fits'.format(self.name, visit)
-            ChipC = ap1d_dir + 'ap1d-{}-{}-c.fits'.format(self.name, visit)
+            ChipA = ap1d_dir + 'ap1d-{}-{}-a.fits'.format(name, visit)
+            ChipB = ap1d_dir + 'ap1d-{}-{}-b.fits'.format(name, visit)
+            ChipC = ap1d_dir + 'ap1d-{}-{}-c.fits'.format(name, visit)
             
             if (os.path.exists(ChipA) == False) or (os.path.exists(ChipB) == False) or (os.path.exists(ChipC) == False) :
-                ap.download(self.name, type='ap1d', visit=visit, frame=1)
+                ap.download(name, type='ap1d', visit=visit, frame=1)
 
-            #get wavelength, flux, and error arrays of spectrum
+            # Get wavelength, flux, and error arrays of spectrum
             hdu_a = fits.open(ChipA)
             t1    = hdu_a[1].data[fiber] # assuming the first extension is a table
             wave1 = hdu_a[4].data[fiber]
@@ -358,9 +273,11 @@ class APOGEE(Spectrum):
             flux3 = t3*1e-17
             err3  = hdu_c[2].data[fiber]
             
-            #concatenate arrays from 3 chips and reverse lists so that lowest wavelength is first
-            self.wave = np.concatenate([wave1, wave2, wave3])[::-1]
-            self.flux = np.concatenate([flux1, flux2, flux3])[::-1]
-            self.error = np.concatenate([err1, err2, err3])[::-1]
+            # Concatenate arrays from 3 chips and reverse lists so that lowest wavelength is first
+            wave = np.concatenate([wave1, wave2, wave3])[::-1]
+            flux = np.concatenate([flux1, flux2, flux3])[::-1]
+            error = np.concatenate([err1, err2, err3])[::-1]
+
+            super().__init__(wave=wave, flux=flux, error=error, name=name)
 
         
