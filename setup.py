5411d269a1cd8b65b91bab291ceafc28b4567041
jbirky <jbirky@ucsd.edu>
2018-05-24 14:07:47 -0700

    add testing

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+import sys
+from setuptools import setup
+
+import apogee_tools
+
+setup(
+    name="apogee_tools",
+    version="0.1",
+    author="Jessica Birky",
+    author_email="jbirky@ucsd.edu",
+    url="http://apogee-tools.readthedocs.io",
+    license="MIT",
+    description=("Forward modeling framework for fitting atmospheric models to stellar spectra."),
+    long_description=open("README.md").read(),
+    install_requires=["numpy", "pandas", "astropy", "astroquery", "emcee", "matplotlib", "pandas", "PyAstronomy", "scipy"],,
+)
