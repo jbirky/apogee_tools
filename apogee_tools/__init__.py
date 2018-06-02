1641c43528948be126f018621ab7f62df57de233
jbirky <jbirky@ucsd.edu>
2018-06-01 21:07:43 -0700

    update

--- apogee_tools/__init__.py
+++ apogee_tools/__init__.py
@@ -1,33 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from .core import Spectrum, Apogee, Nirspec, ModelGrid
-
-from .cannon_tools.run_cannon import labelToSpec, loadLabels, initializeTrainingSet, synthesizeFlux, \
-	runCannon, fitCannonModel, crossValidate, _getPivotsAndScales, _get_lvec, scaleLabels
-from .cannon_tools.plot_cannon import plotCrossValidation, plotCannonModels, plotSpectralSequence, plotBands
-
-from .forward_model.lsf_function import convolveLsf
-from .forward_model.model import initialize, makeModel, returnModelFit
-from .forward_model.rotation_broaden import broaden, applyVsini
-from .forward_model.rv_function import rvShift, rvShiftSpec
-from .forward_model.synthesize_grid import interpolateGrid
-from .forward_model.telluric import applyTelluric, getTelluric
-from .forward_model.read_model import *
-
-from .info.features import lines
-
-from .utils.ap1d import get_1dspec_urls, coadd_epoch, coadd_spectra
-from .utils.continuum import continuum
-from .utils.read import HDF5Convert, HDF5Interface, loadGrid, readModels, getModel
-from .utils.read_lines import listLibraries, listSpecies, searchLines
-from .utils.search import searchStars, searchVisits, download, multiParamSearch, returnSimbadParams, returnAspcapTable
-from .utils.spec_tools import calcScale, compareSpectra, subtractContinuum, integralResample, splineInterpolate
-
-from .instrument_tools.instrument import formatDesignation, getShortname, apogeeFile
-
 import os
 import yaml 
-
 
 # Get directories of apogee_tools 
 PATH 	  = os.path.realpath(__file__)
@@ -60,6 +34,32 @@ step = config["step"]
 prior = config["prior"]
 
 instrument = data["instrument"]
+
+
+from .core import Spectrum, Apogee, Nirspec, ModelGrid
+
+from .cannon_tools.run_cannon import labelToSpec, loadLabels, initializeTrainingSet, synthesizeFlux, \
+	runCannon, fitCannonModel, crossValidate, _getPivotsAndScales, _get_lvec, scaleLabels
+from .cannon_tools.plot_cannon import plotCrossValidation, plotCannonModels, plotSpectralSequence, plotBands
+
+from .forward_model.lsf_function import convolveLsf
+from .forward_model.model import initialize, makeModel, returnModelFit
+from .forward_model.rotation_broaden import broaden, applyVsini
+from .forward_model.rv_function import rvShift, rvShiftSpec
+from .forward_model.synthesize_grid import interpolateGrid
+from .forward_model.telluric import applyTelluric, getTelluric
+from .forward_model.read_model import loadModelParameters, loadModel
+
+from .info.features import lines
+
+from .utils.ap1d import get_1dspec_urls, coadd_epoch, coadd_spectra
+from .utils.continuum import continuum
+from .utils.read import HDF5Convert, HDF5Interface, loadGrid, readModels, getModel
+from .utils.read_lines import listLibraries, listSpecies, searchLines
+from .utils.search import searchStars, searchVisits, download, multiParamSearch, returnSimbadParams, returnAspcapTable
+from .utils.spec_tools import calcScale, compareSpectra, subtractContinuum, integralResample, splineInterpolate
+
+from .instrument_tools.instrument import formatDesignation, getShortname, apogeeFile
 
 
 # import .apogee_hack.spec.lsf as lsf
